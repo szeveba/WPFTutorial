fb163c2683b361ee692830d45e4cd2adcb54ef59
Szegedi Barnabás <szeveba@gmail.com>
2022-09-27 12:07:43 +0200

    hosszú számítás hozzáadva

--- WPFTutorial/MainWindow.xaml.cs
+++ WPFTutorial/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,5 +25,41 @@ namespace WPFTutorial
         {
             InitializeComponent();
         }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.HeightChanged) Label_Height.Content = e.NewSize.Height;
+            if (e.WidthChanged) Label_Width.Content = e.NewSize.Width;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void Calculating_Click(object sender, RoutedEventArgs e)
+        {
+            var calcResult = await Task.Run(Calculating);
+            var result = MessageBox.Show($"Számítás eredménye: {calcResult}\nMegismételjük?", "Eredmény", MessageBoxButton.YesNo, MessageBoxImage.Information);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    Calculating_Click(sender, e);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private int Calculating()
+        {
+            int x = 0;
+            for (int i = 0; i < 1000000000; i++)
+            {
+                x += i;
+            }
+            return x;
+        }
+
     }
 }
