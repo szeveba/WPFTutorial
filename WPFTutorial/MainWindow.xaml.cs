4e2f972a6de1c0ea8df3c66f125dcc0954d31c0d
Szegedi Barnabás <szeveba@gmail.com>
2022-09-27 16:29:18 +0200

    alapok

--- WPFTutorial/MainWindow.xaml.cs
+++ WPFTutorial/MainWindow.xaml.cs
@@ -24,5 +24,77 @@ namespace WPFTutorial
         {
             InitializeComponent();
         }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.WidthChanged) LBL_Width.Content = e.NewSize.Width;
+            if (e.HeightChanged) LBL_Height.Content = e.NewSize.Height;
+        }
+
+        private void Window_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Window_QueryCursor(object sender, QueryCursorEventArgs e)
+        {
+            var point = e.GetPosition(this);
+            LBL_CursorX.Content = point.X;
+            LBL_CursorY.Content = point.Y;
+        }
+
+        private async void BTN_Calculating_Click(object sender, RoutedEventArgs e)
+        {
+            var calcResult = await Task.Run(UselessCalculating);
+            MessageBox.Show(calcResult.ToString(), "Eredmény", MessageBoxButton.OK, MessageBoxImage.Information);
+            var result = MessageBox.Show("Megismételjük?", "Számítás", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    BTN_Calculating_Click(sender, e);
+                    break;
+                    case MessageBoxResult.No:
+                    MainWindow main2 = new MainWindow();
+                    main2.Show();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private decimal UselessCalculating()
+        {
+            decimal d = 0;
+            for (int i = 0; i < 100000000; i++)
+            {
+                d += i;
+            }
+            return d;
+        }
+
+        private void BTN_Add_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(TB_Text.Text))
+            {
+                LB_Text.Items.Add(TB_Text.Text);
+                TB_Text.Clear();
+            }
+        }
+
+        private void TB_Text_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            BTN_Add.IsEnabled = !string.IsNullOrWhiteSpace(TB_Text.Text);
+        }
+
+        private void BTN_Delete_Click(object sender, RoutedEventArgs e)
+        {
+            LB_Text.Items.Remove(LB_Text.SelectedItem);
+            LB_Text.SelectedItem = null;
+        }
+
+        private void LB_Text_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BTN_Delete.IsEnabled = LB_Text.SelectedIndex != -1;
+        }
     }
 }
